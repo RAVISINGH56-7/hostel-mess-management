bbe2742df65c261f03c2a44adf98b0d931d52c00
unknown <ravisingh07234@gmail.com>
2026-07-17 01:24:29 +0530

    Updated hostel food management features

--- src/app/dashboard/student/page.tsx
+++ src/app/dashboard/student/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import DashboardShell from "@/components/DashboardShell";
 import { QRCodeSVG } from "qrcode.react";
